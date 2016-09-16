8d7c6f53640d1679666cd77be1c43a0fe20173ef
dj_coder <jash.kothari@Mirraw.com>
2016-09-16 19:02:04 +0530

    Resizing images for larger screens

--- FacebookAutomatedAds/image_hash.py
+++ FacebookAutomatedAds/image_hash.py
@@ -2,11 +2,21 @@ from facebookads.adobjects.adimage import AdImage
 import header
 import urllib
 import os
+from PIL import Image as Img
+import StringIO
 
 def get_image_hash(url,name,account_id):
 	try:
 		print 'Downloading image'
 		urllib.urlretrieve(url,name)
+		image1 = Img.open('/home/mirrawdev3/ads_automation_facebook/python_trial/Facebook_Ads/'+name)
+		width,height = image1.size
+		size = width if width < height else height
+		# The following is for center cropping
+		image1 = image1.crop(((width/2)-(size/2),(height/2)-(size/2),(width/2)+(size/2),(height/2)+(size/2)))
+		image1 = image1.resize((1080,1080),Img.ANTIALIAS)
+		image1.save(name)
+		image1.close()
 		image = AdImage(parent_id=account_id)
 		image[AdImage.Field.filename] = name
 		print 'Uploading image'
