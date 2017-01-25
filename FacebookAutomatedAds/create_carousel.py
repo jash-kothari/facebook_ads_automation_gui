00394d788b7e2ff043abfa895f6d357dc252ea0a
dj_coder <jash.kothari@mirraw.com>
2017-01-25 17:48:04 +0530

    Bug fix

--- FacebookAutomatedAds/create_carousel.py
+++ FacebookAutomatedAds/create_carousel.py
@@ -1,9 +1,12 @@
+from facebookads.adobjects.adcreativelinkdatacalltoactionvalue import AdCreativeLinkDataCallToActionValue
+from facebookads.adobjects.adcreativelinkdatachildattachment import AdCreativeLinkDataChildAttachment
+from facebookads.adobjects.adcreativelinkdatacalltoaction import AdCreativeLinkDataCallToAction
+from facebookads.adobjects.adcreativeobjectstoryspec import AdCreativeObjectStorySpec
+from facebookads.adobjects.adcreativelinkdata import AdCreativeLinkData
 from facebookads.objects import Ad
 from facebookads.adobjects.campaign import Campaign
 from facebookads.adobjects.adcreative import AdCreative
-from facebookads.adobjects.adcreativelinkdata import AdCreativeLinkData
 from facebookads.adobjects.adcreativeobjectstoryspec import AdCreativeObjectStorySpec
-from facebookads.adobjects.adcreativelinkdatachildattachment import AdCreativeLinkDataChildAttachment
 from time import sleep
 import header
 import my_constants as constants
@@ -22,7 +25,7 @@ def create_carousel_ad(caption,adset_id,ad_name,times,design_list,account_id,lan
 	logger.debug("In create carousel")
 	conn = None
 	simple_list=[]
-	account_medium_list={"act_940036526039709":"fb_ocpc","act_938286879548007":"acpm","act_1010404049002956":"acpm","act_1385041538425866":"acpm","act_1128744890502204":"jcpc","act_10152414205523137":"int","act_972844956092199":"test"}
+	account_medium_list={"act_940036526039709":"fb_ocpc","act_938286879548007":"acpm","act_1010404049002956":"acpm","act_1385041538425866":"acpm","act_1128744890502204":"jcpc","act_10152414205523137":"int","act_972844956092199":"exp"}
 	utm_medium=account_medium_list[account_id]
 	try:
 		urlparse.uses_netloc.append("postgres")
@@ -46,15 +49,25 @@ def create_carousel_ad(caption,adset_id,ad_name,times,design_list,account_id,lan
 			else:
 				product1[AdCreativeLinkDataChildAttachment.Field.link] = url+'?pid='+str(design_id)+'&utm_source=facebook&utm_medium='+utm_medium+'&utm_campaign='+campaign_tag
 			product1[AdCreativeLinkDataChildAttachment.Field.name] = category_name['name']
+			action = AdCreativeLinkDataCallToAction()
+			link_value = AdCreativeLinkDataCallToActionValue()
+			link_value[AdCreativeLinkDataCallToActionValue.Field.link_title] = 'Discount '+str(row['discount_percent'])+'%'
+			action[AdCreativeLinkDataCallToAction.Field.type] = AdCreativeLinkDataCallToAction.Type.shop_now
+			action[AdCreativeLinkDataCallToAction.Field.value] = link_value
+			product1[AdCreativeLinkDataChildAttachment.Field.call_to_action] = action
 			product1[AdCreativeLinkDataChildAttachment.Field.description] = 'Discount '+str(row['discount_percent'])+'%'
 			product1[AdCreativeLinkDataChildAttachment.Field.image_hash] = image_hash.get_image_hash(image_link,rows[1],account_id)
 			sleep(0.5)
 			simple_list.append(product1)
 
 		link = AdCreativeLinkData()
-		link[link.Field.link] = 'www.mirraw.com'
+		link[link.Field.link] = url+'&utm_source=facebook&utm_medium='+utm_medium+'&utm_campaign='+campaign_tag
 		link[link.Field.child_attachments] = simple_list
-		link[link.Field.caption] = caption
+		link[link.Field.caption] = url+'&utm_source=facebook&utm_medium='+utm_medium+'&utm_campaign='+campaign_tag
+		call_to_action_button = AdCreativeLinkDataCallToAction()
+		call_to_action_button[AdCreativeLinkDataCallToAction.Field.type]=AdCreativeLinkDataCallToAction.Type.shop_now
+		link[link.Field.call_to_action] = call_to_action_button
+		link[link.Field.message] = caption
 
 		logger.info(link)
 		story = AdCreativeObjectStorySpec()
@@ -75,7 +88,6 @@ def create_carousel_ad(caption,adset_id,ad_name,times,design_list,account_id,lan
 		ad[Ad.Field.creative] = {'creative_id': str(creative['id'])}
 		logger.info('Creating Ad')
 		ad.remote_create()
-		sleep(15)
 		logger.info(ad)
 
 	except psycopg2.DatabaseError, e:
--- FacebookAutomatedAds/header.py
+++ FacebookAutomatedAds/header.py
@@ -6,14 +6,62 @@ from facebookads.adobjects.adaccount import AdAccount
 from facebookads.adobjects.adaccountuser import AdAccountUser
 import os
 import my_constants as constants
+from time import sleep
+import logging
+import urlparse
+import psycopg2
+import psycopg2.extras
+import itertools
+import image_hash
 #Initialize a new Session and instantiate an API object:
 FacebookAdsApi.init(constants.my_app_id, constants.my_app_secret, constants.my_access_token)
+logger = logging.getLogger('testLogger')
 def get_ids():
 	me = AdUser(fbid='me')
 	my_account=me.get_ad_accounts(fields=[AdAccount.Field.name,AdAccount.Field.id])
 	id_list={}
 	for i in xrange(len(my_account)):
 		id_list[str(my_account[i][AdAccount.Field.id])] = str(my_account[i][AdAccount.Field.name])
-	print id_list
-	sleep(5)
+	logger.info(id_list)
 	return id_list
+
+def get_category_list():
+	urlparse.uses_netloc.append("postgres")
+	database_url = urlparse.urlparse(constants.database_url)
+	conn = psycopg2.connect( database=database_url.path[1:], user=database_url.username, password=database_url.password, host=database_url.hostname, port=database_url.port )
+	curr = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+	curr.execute('select id,name from categories')
+	rows = curr.fetchall()
+	category_hash={}
+	conn.close()
+	for row in rows:
+		category_hash[int(row['id'])] = row['name']
+	return category_hash
+
+def get_top_selling_items(category_name):
+	urlparse.uses_netloc.append("postgres")
+	database_url = urlparse.urlparse(constants.database_url)
+	conn = psycopg2.connect( database=database_url.path[1:], user=database_url.username, password=database_url.password, host=database_url.hostname, port=database_url.port )
+	curr = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+	id_list=[]
+	curr.execute("select id from categories where name ='"+category_name+"'")
+	row=curr.fetchone()
+	id_list.append(row['id'])
+	curr.execute("select id from categories where parent_id ="+str(id_list[0]))
+	rows=curr.fetchall()
+	for id in rows:
+		id_list.append(id['id'])
+	curr.execute("SELECT l.design_id FROM line_items l,categories_designs cd,categories c,designs d WHERE cd.design_id=l.design_id AND c.id=cd.category_id AND l.design_id = d.id AND lower(d.state) like 'in_stock' AND l.created_at > current_date - interval '90' day and c.id in ("+','.join(map(str, id_list))+") GROUP BY l.design_id,c.name ORDER BY count(l.id) DESC LIMIT 100")
+	design_ids = rows = list(set(list(itertools.chain.from_iterable(curr.fetchall()))))
+	top_selling_list=[]
+	top_selling_hash={}
+	for design_id in design_ids:
+		curr.execute('SELECT id,photo_file_name FROM images where design_id = '+str(design_id))
+		rows = curr.fetchone()
+		image_link = image_hash.get_image_link(rows['photo_file_name'],rows['id'],'_thumb')
+		curr.execute('SELECT discount_percent from designs where id='+str(design_id))
+		row = curr.fetchone()
+		internal_hash = {'image_link': image_link,'discount': row['discount_percent'],'design_id': design_id}
+		top_selling_list.append(internal_hash)
+	top_selling_hash['top_items']=top_selling_list
+	return top_selling_hash
